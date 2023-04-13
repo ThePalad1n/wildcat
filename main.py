1ab7b2308ee1dd97cd983e3934885f690b910423
Scott Shanfeld <74272645+scottshanfeld@users.noreply.github.com>
2023-04-13 11:30:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import re
 
+
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.left = None
         self.right = None
 
-class BSTParser:
+class BST:
     def __init__(self, tokens):
         self.tokens = tokens
         self.index = 0
+        self.root = None
+        self.height = 0
+        self.size = 0
 
     def parse(self):
         root = self._parse_expression()
@@ -56,23 +60,127 @@ class BSTParser:
             return node
         else:
             raise ValueError(f"Unexpected token {token} at position {self.index}")
+        
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+            self.size += 1
+            return
+        current = self.root
+        parent = None
+        while current is not None:
+            parent = current
+            if data < current.data:
+                current = current.left
+            else:
+                current = current.right
+        if data < parent.data:
+            parent.left = Node(data)
+        else:
+            parent.right = Node(data)
+        self.size += 1
+        self.height = max(self.height, self.get_height(self.root))
+
+    def delete(self, data):
+        self.root = self.delete_helper(self.root, data)
+        self.size -= 1
+        self.height = max(self.get_height(self.root), 0)
+
+    def delete_helper(self, node, data):
+        if node is None:
+            return None
+        if data < node.data:
+            node.left = self.delete_helper(node.left, data)
+        elif data > node.data:
+            node.right = self.delete_helper(node.right, data)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                temp = self.get_min(node.right)
+                node.data = temp.data
+                node.right = self.delete_helper(node.right, temp.data)
+        return node
+
+    def get_min(self, node):
+        while node.left is not None:
+            node = node.left
+        return node
+
+    def search(self, data):
+        current = self.root
+        while current is not None:
+            if data == current.data:
+                return True
+            elif data < current.data:
+                current = current.left
+            else:
+                current = current.right
+        return False
+
+    def preorder_iterate(self, node):
+        if node is not None:
+            print(node.data, end=" ")
+            self.preorder_iterate(node.left)
+            self.preorder_iterate(node.right)
+
+    def inorder_iterate(self, node):
+        if node is not None:
+            self.inorder_iterate(node.left)
+            print(node.data, end=" ")
+            self.inorder_iterate(node.right)
+
+    def postorder_iterate(self, node):
+        if node is not None:
+            self.postorder_iterate(node.left)
+            self.postorder_iterate(node.right)
+            print(node.data, end=" ")
+
+    def value_order_iterate(self, node):
+        if node is not None:
+            self.value_order_iterate(node.left)
+            print(node.data, end=" ")
+            self.value_order_iterate(node.right)
+
+    def get_height(self, node):
+        if node is None:
+            return 0
+        else:
+            return 1 + max(self.get_height(node.left), self.get_height(node.right))
+        
+    def get_node_depth(self, root, target_node):
+        if root is None:
+            return None
+        elif root == target_node:
+            return 0
+        else:
+            left_depth = self.get_node_depth(root.left, target_node)
+            if left_depth is not None:
+                return left_depth + 1
+            right_depth = self.get_node_depth(root.right, target_node)
+            if right_depth is not None:
+                return right_depth + 1
+            return None
+
 
 def evaluate(node):
     if node.left is None and node.right is None:
-        return node.value.get_value(node.value.value)
+        return node.data.get_value(node.data.value)
     else:
         left = evaluate(node.left)
         right = evaluate(node.right)
-        if node.value.token_type == "PLUS":
+        if node.data.token_type == "PLUS":
             return left + right
-        elif node.value.token_type == "MINUS":
+        elif node.data.token_type == "MINUS":
             return left - right
-        elif node.value.token_type == "MULTIPLY":
+        elif node.data.token_type == "MULTIPLY":
             return left * right
-        elif node.value.token_type == "DIVIDE":
+        elif node.data.token_type == "DIVIDE":
             return left / right
         else:
-            raise ValueError(f"Unknown operator {node.value.token_type}")
+            raise ValueError(f"Unknown operator {node.data.token_type}")
 
 categorized ={
 'literals': [r'\d+|\'.*?\'|\".*?\"'],
@@ -142,14 +250,45 @@ def lex(input_string):
 
 
 
-
-with open('source_code.txt', 'r') as file:
-    code = file.read()
+#with open('source_code.txt', 'r') as file:
+ #   code = file.read()
 # Test the lexer
+
+bst = BST([])
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+# print the tree using various traversal methods
+print("Preorder traversal:")
+bst.preorder_iterate(bst.root)
+print("\nInorder traversal:")
+bst.inorder_iterate(bst.root)
+print("\nPostorder traversal:")
+bst.postorder_iterate(bst.root)
+print("\nValue order traversal:")
+bst.value_order_iterate(bst.root)
+
+# search for a node
+print("\nSearch for 60:", bst.search(60))
+print("Search for 10:", bst.search(10))
+
+# delete a node and print the tree again
+bst.delete(50)
+print("After deleting 50:")
+bst.inorder_iterate(bst.root)
+print("\n")
+
+
+code = "12.1 plus 34 times (56.0 minus 78.97 divide 90)"
 input_string = code
 tokens = lex(input_string)
 print(tokens)
-parser = BSTParser(tokens)
+parser = BST(tokens)
 root = parser.parse()
 result = evaluate(root)
 print(result)
