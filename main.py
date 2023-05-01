6052596eefcc32bf33f4e464139c22956ce79fa9
ThePalad1n <evangreen91@gmail.com>
2023-05-01 00:28:32 -0400

    commented

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import re
 
-
+#Defines the node class
 class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+#Defines the Binary Search Tree(BST) Class
 class BST:
     def __init__(self, tokens):
         self.tokens = tokens
@@ -15,12 +16,14 @@ class BST:
         self.height = 0
         self.size = 0
 
+    # Main parse method to parse the input expression
     def parse(self):
         root = self._parse_expression()
         if self.index < len(self.tokens):
             raise ValueError(f"Unexpected token {self.tokens[self.index]} at position {self.index}")
         return root
 
+    # Helper method to parse an expression
     def _parse_expression(self):
         left = self._parse_term()
         while self.index < len(self.tokens) and self.tokens[self.index].token_type in ["PLUS", "MINUS"]:
@@ -33,6 +36,7 @@ class BST:
             left = node
         return left
 
+    # Helper method to parse a term
     def _parse_term(self):
         left = self._parse_factor()
         while self.index < len(self.tokens) and self.tokens[self.index].token_type in ["MULTIPLY", "DIVIDE"]:
@@ -45,6 +49,7 @@ class BST:
             left = node
         return left
 
+    # Helper method to parse a factor
     def _parse_factor(self):
         if self.index >= len(self.tokens):
             raise ValueError(f"Unexpected end of expression")
@@ -60,7 +65,8 @@ class BST:
             return node
         else:
             raise ValueError(f"Unexpected token {token} at position {self.index}")
-        
+
+    # Method to insert a value into the BST
     def insert(self, data):
         if self.root is None:
             self.root = Node(data)
@@ -81,11 +87,13 @@ class BST:
         self.size += 1
         self.height = max(self.height, self.get_height(self.root))
 
+    # Method to delete a value from the BST
     def delete(self, data):
         self.root = self.delete_helper(self.root, data)
         self.size -= 1
         self.height = max(self.get_height(self.root), 0)
 
+    # Helper method for deleting a node with a specific value
     def delete_helper(self, node, data):
         if node is None:
             return None
@@ -104,11 +112,13 @@ class BST:
                 node.right = self.delete_helper(node.right, temp.data)
         return node
 
+    # Method to get the minimum value in the BST
     def get_min(self, node):
         while node.left is not None:
             node = node.left
         return node
 
+    # Method to search for a value in the BST
     def search(self, data):
         current = self.root
         while current is not None:
@@ -120,36 +130,42 @@ class BST:
                 current = current.right
         return False
 
+    # Traversal method for preorder
     def preorder_iterate(self, node):
         if node is not None:
             print(node.data, end=" ")
             self.preorder_iterate(node.left)
             self.preorder_iterate(node.right)
 
+    # Traversal method for inorder
     def inorder_iterate(self, node):
         if node is not None:
             self.inorder_iterate(node.left)
             print(node.data, end=" ")
             self.inorder_iterate(node.right)
 
+    # Traversal method for postorder
     def postorder_iterate(self, node):
         if node is not None:
             self.postorder_iterate(node.left)
             self.postorder_iterate(node.right)
             print(node.data, end=" ")
 
+    # Traversal method for value order
     def value_order_iterate(self, node):
         if node is not None:
             self.value_order_iterate(node.left)
             print(node.data, end=" ")
             self.value_order_iterate(node.right)
 
+    # Method to get the height of the BST
     def get_height(self, node):
         if node is None:
             return 0
         else:
             return 1 + max(self.get_height(node.left), self.get_height(node.right))
-        
+
+    # Method to get the depth of a specific node in the BST
     def get_node_depth(self, root, target_node):
         if root is None:
             return None
@@ -164,7 +180,7 @@ class BST:
                 return right_depth + 1
             return None
 
-
+# Function to evaluate the result of the parsed expression
 def evaluate(node):
     if node.left is None and node.right is None:
         return node.data.get_value(node.data.value)
@@ -182,6 +198,7 @@ def evaluate(node):
         else:
             raise ValueError(f"Unknown operator {node.data.token_type}")
 
+# Define the categorization of language elements
 categorized ={
 'literals': [r'\d+|\'.*?\'|\".*?\"'],
 'operators': [r'\plus|\minus|\times|\divide'],
@@ -212,6 +229,7 @@ TOKEN_TYPES = [
     ("LESSTHANORIS", r"less\sthan\sor\sis"),
 ]
 
+# Define the Token class
 class Token:
     def __init__(self, token_type, value):
         self.token_type = token_type
@@ -230,6 +248,7 @@ class Token:
     def __repr__(self):
         return f"Token({self.token_type}, {self.value})"
 
+# Function to perform lexical analysis (tokenization)
 def lex(input_string):
     tokens = []
     position = 0
@@ -250,41 +269,41 @@ def lex(input_string):
 
 
 
-#with open('source_code.txt', 'r') as file:
- #   code = file.read()
+with open('source_code.txt', 'r') as file:
+   code = file.read()
 # Test the lexer
 
 bst = BST([])
-bst.insert(50)
-bst.insert(30)
-bst.insert(70)
-bst.insert(20)
-bst.insert(40)
-bst.insert(60)
-bst.insert(80)
+# bst.insert(50)
+# bst.insert(30)
+# bst.insert(70)
+# bst.insert(20)
+# bst.insert(40)
+# bst.insert(60)
+# bst.insert(80)
+#
+# # print the tree using various traversal methods
+# print("Preorder traversal:")
+# bst.preorder_iterate(bst.root)
+# print("\nInorder traversal:")
+# bst.inorder_iterate(bst.root)
+# print("\nPostorder traversal:")
+# bst.postorder_iterate(bst.root)
+# print("\nValue order traversal:")
+# bst.value_order_iterate(bst.root)
+#
+# # search for a node
+# print("\nSearch for 60:", bst.search(60))
+# print("Search for 10:", bst.search(10))
+#
+# # delete a node and print the tree again
+# bst.delete(50)
+# print("After deleting 50:")
+# bst.inorder_iterate(bst.root)
+# print("\n")
 
-# print the tree using various traversal methods
-print("Preorder traversal:")
-bst.preorder_iterate(bst.root)
-print("\nInorder traversal:")
-bst.inorder_iterate(bst.root)
-print("\nPostorder traversal:")
-bst.postorder_iterate(bst.root)
-print("\nValue order traversal:")
-bst.value_order_iterate(bst.root)
 
-# search for a node
-print("\nSearch for 60:", bst.search(60))
-print("Search for 10:", bst.search(10))
-
-# delete a node and print the tree again
-bst.delete(50)
-print("After deleting 50:")
-bst.inorder_iterate(bst.root)
-print("\n")
-
-
-code = "12.1 plus 34 times (56.0 minus 78.97 divide 90)"
+# code = "12.1 plus 34 times (56.0 minus 78.97 divide 90)"
 input_string = code
 tokens = lex(input_string)
 print(tokens)
